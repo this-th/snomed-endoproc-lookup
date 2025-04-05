4664d5f605179b08c373a6bf52be1794d6a5bb98
Rath Panyowat <rath.panyowat@outlook.com>
2025-04-05 18:00:55 +0700

    Ready to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/snomed-endoproc-lookup' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
